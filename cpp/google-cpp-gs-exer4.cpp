9f58c82120d28adc9a2b37b1046d89eb539244db
Nejdet Eren Pinaz <erenpinaz@pop-os.localdomain>
2019-11-03 19:41:57 +0300

    Exercise#4 added (incomplete)

---
+++ cpp/google-cpp-gs-exer4.cpp
@@ -0,0 +1,16 @@
+// google-cpp-gs-ex4.cpp: Eren Pinaz
+// Description: Google cpp Exercise#4
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void F() { cout << "FFFFF\nF\nFFF\nF\nF\n" << endl; }
+
+void E() { cout << "EEEEE\nE\nEEEEE\nE\nEEEEE\n" << endl; }
+
+int main(int argc, char const *argv[]) {
+    F();
+    E();
+    return 0;
+}
