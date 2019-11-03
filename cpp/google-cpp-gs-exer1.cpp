d5db937f50fdbc28af33447ff1fd1ca2e8a656be
Nejdet Eren Pinaz <erenpinaz@pop-os.localdomain>
2019-11-03 19:10:45 +0300

    Exercise#1 added

---
+++ cpp/google-cpp-gs-exer1.cpp
@@ -0,0 +1,14 @@
+// google-cpp-gs-ex2.cpp: Eren Pinaz
+// Description: Google cpp Exercise#1
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double input_chirps;
+    cout << "Enter number of chirps: "; cin >> input_chirps;
+    cout << "The temperature is: " << (double)((input_chirps + 40) / 4) << " degrees" << endl;
+
+    return 0;
+}
